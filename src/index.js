fdbd8cef180948108a431ee7f3f466ce5c81c672
Raymond Gregory <vinedsel@gmail.com>
2019-01-29 16:37:07 -0600

    Finished code, doesn't work, will submit to see actual solution page, then i'll paste in the actual solution down underneath my own code.

--- src/index.js
+++ src/index.js
@@ -75,3 +75,46 @@ class Photo extends react.Component {
 }
 
 render(<App />, document.getElementById('root'));
+
+
+
+//________________________________________________________
+
+// Actual Solution, boy did i screw this up...
+
+import React from "react";
+import { render } from "react-dom";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+let APIUrl = "https://jsonplaceholder.typicode.com/photos";
+class App extends React.Component {
+  state = {
+    pictures: [],
+    errors: null
+  };
+
+  componentDidMount() {
+    fetch(APIUrl)
+      .then(rep => rep.json())
+      .then(payload => {
+        let first50 = payload.filter((i, index) => index < 50);
+        console.log(first50);
+        this.setState({pictures: first50})
+      })
+      .catch(err => this.setState({ error: err }));
+  }
+
+  render() {
+    return (
+      <div style={styles}>
+        {this.state.pictures.map(pic => (
+          <img key={pic.id} src={pic.thumbnailUrl} alt={pic.title}/>
+        ))}
+      </div>
+    );
+  }
+}
+render(<App />, document.getElementById("root"));
